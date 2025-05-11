cfe8187cea73096b4b9c4d937e6356460ad98b37
Neo Phetaone <phetoaneneo4@gmail.com>
2025-05-11 22:53:13 +0200

    Information pages (with place holders only) added along with their routes.

--- indabax-equatorial-guinea/src/App.jsx
+++ indabax-equatorial-guinea/src/App.jsx
@@ -2,6 +2,10 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Speakers from "./pages/Speakers";
+import Schedule from "./pages/Schedule";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/speakers" element={<Speakers />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
