30a8644a2ce7810f9693c84e837e971186ce4cca
Neo Phetaone <phetoaneneo4@gmail.com>
2025-05-11 22:39:45 +0200

    Home page with place holder added and its path added to App.jsx

--- indabax-equatorial-guinea/src/App.jsx
+++ indabax-equatorial-guinea/src/App.jsx
@@ -1,11 +1,16 @@
 import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
