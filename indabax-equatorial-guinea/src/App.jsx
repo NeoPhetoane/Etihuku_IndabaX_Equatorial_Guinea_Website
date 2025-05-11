c41816d22af4cbabc466ca4e0b5b2bf229172019
Neo Phetaone <phetoaneneo4@gmail.com>
2025-05-11 19:37:22 +0200

    React, Vite and Tailwind setup.

---
+++ indabax-equatorial-guinea/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <>
+    <div className="bg-red-800">
+    <p className="text-red-700">wow</p>
+    </div>
+    </>
+  )
+}
+
+export default App
